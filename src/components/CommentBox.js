3bfe9ec5dca6883f31342c0e6279f83f47208b05
Mike Wong <wongmdesign@gmail.com>
2019-04-02 16:57:01 -0400

    refactored code and modified commentbox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return <div>Box for comment</div>
+export default class CommentBox extends Component {
+  state = { comment: '' }
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    // TODO: call an action creator and save comment
+    this.setState({ comment: '' })
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea
+          name=''
+          id=''
+          onChange={this.handleChange}
+          value={this.state.comment}
+          cols='30'
+          rows='10'
+        />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
 }
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 // absolute path modified from .env file
 import App from 'components/App'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
-// this code also located in setupTests.js
-// Added here so Jest auto-test runs without error
-import Adapter from 'enzyme-adapter-react-16'
-Enzyme.configure({ adapter: new Adapter() })
+// initialize wrapped so it is accessible in each of the tests since they share common setup with App
+let wrapped
+
+beforeEach(() => {
+  // shallow renders just app component and no children inside of it
+  wrapped = shallow(<App />)
+})
 
 it('shows a comment box', () => {
-  // shallow renders just app component and no children inside of it
-  const wrapped = shallow(<App />)
-
   // this returns array of CommentBox components
   expect(wrapped.find(CommentBox).length).toEqual(1)
 })
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />)
   expect(wrapped.find(CommentList).length).toEqual(1)
 })
