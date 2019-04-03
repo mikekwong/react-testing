30aa6b825f7f308847bbd874cebe08ea682d6aac
Mike Wong <wongmdesign@gmail.com>
2019-04-02 21:33:27 -0400

    text area tests refactored

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -21,28 +21,30 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1)
 })
 
-// simulate events for text
-it('has a text area that users can type in', () => {
-  // find the textarea element
-  // simulate an event by specifying the html event name, provide a fake/mock event object to handleChange handler to simulate value
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+describe('the text area', () => {
+  // 1st
+  beforeEach(() => {
+    // find the textarea element
+    // simulate an event by specifying the html event name, provide a fake/mock event object to handleChange handler to simulate value
+    // these were extracted from the bottom 2 tests rather than repeating it twice.
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    })
+    // force a re-render as handleChange state changes are async
+    wrapped.update()
   })
-  // force a re-render as handleChange state changes are async
-  wrapped.update()
-
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
-})
-
-it('when form is submitted, text area gets emptied', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+  // 2nd
+  // simulate events for text
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
   })
-  wrapped.update()
-  // to submit the form. simulate name of html event
-  wrapped.find('form').simulate('submit')
-  // update the form again after the simulation above happens with submit so state change can happen
-  wrapped.update()
-  // after submission, we expect the text area state value to equal empty string
-  expect(wrapped.find('textarea').prop('value')).toEqual('')
+  // 3rd
+  it('when form is submitted, text area gets emptied', () => {
+    // to submit the form. simulate name of html event
+    wrapped.find('form').simulate('submit')
+    // update the form again after the simulation above happens with submit so state change can happen
+    wrapped.update()
+    // after submission, we expect the text area state value to equal empty string
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  })
 })
