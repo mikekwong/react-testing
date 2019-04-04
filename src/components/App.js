324e326cad32b86c727e8176543ee53379d8fc3c
Mike Wong <wongmdesign@gmail.com>
2019-04-04 16:00:17 -0400

    HOC set for CommentBox

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,52 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from 'actions'
 // absolute path modified from .env file
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
-export default () => {
-  return (
-    <div>
-      <Route path='/post' component={CommentBox} />
-      <Route exact path='/' component={CommentList} />
-    </div>
-  )
+class App extends Component {
+  renderButton () {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+      )
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+      )
+    }
+  }
+  renderHeader () {
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    )
+  }
+  render () {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path='/post' component={CommentBox} />
+        <Route exact path='/' component={CommentList} />
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(
+  mapStateToProps,
+  actions
+)(App)
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from 'actions'
+// imoprt HOC to use with chosen component
+import requireAuth from 'components/requireAuth'
 
 class CommentBox extends Component {
   state = { comment: '' }
+
+  // // Our component just got rendered
+  // componentDidMount () {
+  //   this.shouldNavigateAway()
+  // }
+  // // Our component just got updated
+  // componentDidUpdate () {
+  //   this.shouldNavigateAway()
+  // }
+
+  // shouldNavigateAway () {
+  //   // once signed out state changes, redirect to root
+  //   if (!this.props.auth) {
+  //     this.props.history.push('/')
+  //   }
+  // }
 
   handleChange = e => {
     this.setState({ comment: e.target.value })
@@ -16,7 +34,7 @@ class CommentBox extends Component {
     this.setState({ comment: '' })
   }
 
-  render () {
+  render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -24,15 +42,15 @@ class CommentBox extends Component {
           <textarea
             onChange={this.handleChange}
             value={this.state.comment}
-            cols='30'
-            rows='10'
+            cols="30"
+            rows="10"
           />
           <div>
             <button>Submit Comment</button>
           </div>
         </form>
         {/* button added outside of form to it doesn't get caught up in submission of actual form */}
-        <button className='fetch-comments' onClick={this.props.fetchComments}>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
           Fetch Comments
         </button>
       </div>
@@ -40,7 +58,13 @@ class CommentBox extends Component {
   }
 }
 
+// const mapStateToProps = state => {
+//   const { auth } = state
+//   return { auth }
+// }
+
+// for connect with use of HOC, pass down the HOC with the component as an argument VS just the component itself
 export default connect(
   null,
   actions
-)(CommentBox)
+)(requireAuth(CommentBox))
